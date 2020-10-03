ec5b3d2f49a6826bc0134a0feb45fd2b791f752c
Roger Luo <rawger@berkeley.edu>
2020-10-02 20:08:56 -0700

    Initial Commit

---
+++ SwiftyNarou/SwiftyNarou.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyNarou.h
+//  SwiftyNarou
+//
+//  Created by Roger Luo on 10/2/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyNarou.
+FOUNDATION_EXPORT double SwiftyNarouVersionNumber;
+
+//! Project version string for SwiftyNarou.
+FOUNDATION_EXPORT const unsigned char SwiftyNarouVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyNarou/PublicHeader.h>
+
+
