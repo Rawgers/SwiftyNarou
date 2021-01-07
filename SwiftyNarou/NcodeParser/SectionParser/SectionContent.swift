0dcb6554acdd1ef09e16833628c0d7691db74f56
Roger Luo <rawger@berkeley.edu>
2021-01-07 14:51:33 -0800

    Add public init for SectionContent.

--- SwiftyNarou/NcodeParser/SectionParser/SectionContent.swift
+++ SwiftyNarou/NcodeParser/SectionParser/SectionContent.swift
@@ -15,4 +15,26 @@ public struct SectionContent {
     public let prevNcode: String?
     public let nextNcode: String?
     public let progress: String
+    
+    public init(
+        sectionTitle: String,
+        chapterTitle: String?,
+        novelTitle: String,
+        writer: String,
+        content: String,
+        format: [NSRange : String],
+        prevNcode: String?,
+        nextNcode: String?,
+        progress: String
+    ) {
+        self.sectionTitle = sectionTitle
+        self.chapterTitle = chapterTitle
+        self.novelTitle = novelTitle
+        self.writer = writer
+        self.content = content
+        self.format = format
+        self.prevNcode = prevNcode
+        self.nextNcode = nextNcode
+        self.progress = progress
+    }
 }
